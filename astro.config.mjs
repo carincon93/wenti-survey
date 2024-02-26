7fa7f871c23b0486d5f17bd99260aea532ac6cb3
cris <carincon93@gmail.com>
2024-02-26 09:38:14 -0500

    Project initialized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    // output: 'server',
+    output: 'server',
     integrations: [react(), tailwind()],
+    adapter: vercel(),
 })
