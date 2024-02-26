201bfaf4bd8e6ec25481c3cffd77acf28e2b1291
cris <carincon93@gmail.com>
2024-02-26 09:42:20 -0500

    Project initialized

--- src/utils/xata.ts
+++ src/utils/xata.ts
@@ -1,4 +1,4 @@
 // This would allow you to wrap the generated client from src/xata.ts with your own credentials
 import { XataClient } from '@/xata.ts'
 
-export const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY, branch: import.meta.env.XATA_BRANCH })
+export const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY ?? '', branch: import.meta.env.XATA_BRANCH })
